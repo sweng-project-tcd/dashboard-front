390e0760f80b868c2cc4eb16178cd914da448ca5
Neil Shevlin <neilshevlin.98@gmail.com>
2022-11-17 13:08:39 +0000

    Added Main Dashboard Layout

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -21,7 +21,7 @@ export default function DashboardLayout({ children }) {
                 }}
               >
       
-                <Main>{children}</Main>
+                {children}
               </Box>
             </>
           );
