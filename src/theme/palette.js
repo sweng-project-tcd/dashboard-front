db256d85584bcc1d5dd7133410d13eb9cc9b4272
Neil Shevlin <neilshevlin.98@gmail.com>
2022-11-22 17:05:26 +0000

    Added app pallete

---
+++ src/theme/palette.js
@@ -0,0 +1,131 @@
+import { alpha } from '@mui/material/styles';
+
+// ----------------------------------------------------------------------
+
+// SETUP COLORS
+
+const GREY = {
+  0: '#FFFFFF',
+  100: '#F9FAFB',
+  200: '#F4F6F8',
+  300: '#DFE3E8',
+  400: '#C4CDD5',
+  500: '#919EAB',
+  600: '#637381',
+  700: '#454F5B',
+  800: '#212B36',
+  900: '#161C24',
+};
+const PRIMARY = {
+  lighter: '#ebfdff',
+  light: '#ccf9ff',
+  main: '#8ef2ff',
+  dark: '#4ed8e8',
+  darker: '#20bccc',
+  contrastText: '#191c1d',
+};
+
+
+const SECONDARY = {
+  lighter: '#ebfdff',
+  light: '#dbf6fa',
+  main: '#cde7eb',
+  dark: '#b1cbcf',
+  darker: '#96b0b4',
+  contrastText: '##051f22',
+};
+
+const INFO = {
+  lighter: '#8390b2',
+  light: '#6a7797',
+  main: '#515e7d',
+  dark: '#455271',
+  darker: '#394664',
+  contrastText: '#fff',
+};
+
+const SUCCESS = {
+  lighter: '#D8FBDE',
+  light: '#86E8AB',
+  main: '#36B37E',
+  dark: '#1B806A',
+  darker: '#0A5554',
+  contrastText: '#fff',
+};
+
+const WARNING = {
+  lighter: '#FFF5CC',
+  light: '#FFD666',
+  main: '#FFAB00',
+  dark: '#B76E00',
+  darker: '#7A4100',
+  contrastText: GREY[800],
+};
+
+const ERROR = {
+  lighter: '#FFE9D5',
+  light: '#FFAC82',
+  main: '#FF5630',
+  dark: '#B71D18',
+  darker: '#7A0916',
+  contrastText: '#fff',
+};
+
+const COMMON = {
+  common: { black: '#000', white: '#fff' },
+  primary: PRIMARY,
+  secondary: SECONDARY,
+  info: INFO,
+  success: SUCCESS,
+  warning: WARNING,
+  error: ERROR,
+  grey: GREY,
+  divider: alpha(GREY[500], 0.24),
+  action: {
+    hover: alpha(GREY[500], 0.08),
+    selected: alpha(GREY[500], 0.16),
+    disabled: alpha(GREY[500], 0.8),
+    disabledBackground: alpha(GREY[500], 0.24),
+    focus: alpha(GREY[500], 0.24),
+    hoverOpacity: 0.08,
+    disabledOpacity: 0.48,
+  },
+};
+
+export default function palette(themeMode) {
+  const light = {
+    ...COMMON,
+    mode: 'light',
+    text: {
+      primary: GREY[800],
+      secondary: SECONDARY.darker,
+      disabled: GREY[500],
+    },
+    background: { paper: '#fff', default: '#fff', neutral: GREY[200] },
+    action: {
+      ...COMMON.action,
+      active: GREY[600],
+    },
+  };
+
+  const dark = {
+    ...COMMON,
+    mode: 'dark',
+    text: {
+      primary: '#fff',
+      secondary: SECONDARY.lighter,
+      disabled: GREY[600],
+    },
+    background: {
+      paper: GREY[800],
+      default: GREY[900],
+      neutral: alpha(GREY[500], 0.16),
+    },
+    action: {
+      ...COMMON.action,
+      active: GREY[500],
+    },
+  };
+
+  return themeMode === 'light' ? light : dark;
+}
