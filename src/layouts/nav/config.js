944fcf8a269117d7dec75dd09c7460a89bcf3ac6
Neil Shevlin <neilshevlin.98@gmail.com>
2022-11-28 09:57:53 +0000

    Added a reroute config file for dash board

---
+++ src/layouts/nav/config.js
@@ -0,0 +1,15 @@
+// routes
+import { PATH_DASHBOARD } from '../../routes/paths';
+const navConfig = [
+  // GENERAL
+  // ----------------------------------------------------------------------
+  {
+    subheader: 'Dahsboard',
+    items: [
+      { title: 'Home', path: PATH_DASHBOARD.home },
+
+    ],
+  },
+];
+
+export default navConfig;
