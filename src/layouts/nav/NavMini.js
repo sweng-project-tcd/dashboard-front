ed64084dcbc183216d8e80f8d0aa25c6d2039e11
Neil Shevlin <neilshevlin.98@gmail.com>
2022-11-28 09:58:30 +0000

    added side nav components to the layout

---
+++ src/layouts/nav/NavMini.js
@@ -0,0 +1,41 @@
+// @mui
+import { Stack, Box, Typography } from '@mui/material';
+//
+import navConfig from './config';
+
+// ----------------------------------------------------------------------
+
+const NAV = {
+  W_BASE: 260,
+  W_DASHBOARD: 280,
+  W_DASHBOARD_MINI: 88,
+  //
+  H_DASHBOARD_ITEM: 48,
+  H_DASHBOARD_ITEM_SUB: 36,
+  //
+  H_DASHBOARD_ITEM_HORIZONTAL: 32,
+};
+export default function NavMini() {
+  return (
+    <Box
+      component="nav"
+      sx={{
+        flexShrink: { lg: 0 },
+        width: { lg: NAV.W_DASHBOARD_MINI },
+      }}
+    >
+      <Stack
+        sx={{
+          pb: 2,
+          height: 1,
+          position: 'fixed',
+          width: NAV.W_DASHBOARD_MINI,
+          borderRight: (theme) => `dashed 1px ${theme.palette.divider}`,
+        }}
+      >
+        <Typography> Logo </Typography>
+
+      </Stack>
+    </Box>
+  );
+}
