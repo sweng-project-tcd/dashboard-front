7cf6ed7ea312dd41198a69b7eaf0f5fcb5dcd41a
Neil Shevlin <neilshevlin.98@gmail.com>
2022-12-02 10:16:41 +0000

    Added axios util for making API calls

---
+++ src/utils/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({baseURL: 'http://localhost:8000'});
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+  );
+  
+export default axiosInstance;
+  
